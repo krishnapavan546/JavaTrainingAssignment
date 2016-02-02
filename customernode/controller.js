f835d4fe40f0c915e6dad6eca4a088d8e25e7796
krishnapavan546 <krishnapavan546@gmail.com>
2016-02-02 12:24:05 +0530

    Update controller.js
    Commiting URL Changes

--- customernode/controller.js
+++ customernode/controller.js
@@ -4,7 +4,7 @@ var express = require('express');
 const browser = require('./clientService.js');
 
 var app = express();
-app.get('/getById',function(req,res){
+app.get('/getProductId',function(req,res){
   browser.handler(req,res);
 });
 app.listen(8090,function(){});
